cad2170447babe113ed13994a1bb555644604b7a
Taylor Welling <tw3080@gmail.com>
2016-12-13 02:30:31 -0800

    Adding/removing nav bar box shadow class based on scroll position; mobile dropdown menu now properly closes when anchor links are clicked

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 $(document).on('scroll', function(event) {
-    var scrollPos = $(document).scrollTop() + 81;
+    if ($(document).scrollTop() > 0) {
+        $('.nav').addClass('js-nav-shadow');
+    } else {
+        $('.nav').removeClass('js-nav-shadow');
+    }
+
+    // var scrollPos = $(document).scrollTop() + 81;
+    var scrollPos = $(document).scrollTop() + 128;
     $('.nav-menu a').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
-        var bottom = ((scrollPos - 81 + $(window).height()) == $(document).height());
+        var bottom = ((scrollPos - 128 + $(window).height()) == $(document).height());
 
-        //  The purpose of this outer if/else statement is to fix an issue with the 'Contact' link never becoming active because the window is generally too tall
+        //  The purpose of this outer if/else statement is to fix an issue with the 'Contact' nav link never becoming active because the window is generally too tall
         // If the user hasn't scrolled to the bottom, switch between making the 'About' and 'Portfolio' links active depending on scroll position
         if (!bottom) {
             if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
@@ -23,11 +30,18 @@ $(document).on('scroll', function(event) {
     });
 });
 
+// On mobile, always add the box shadow to the nav if the dropdown menu is visible
+$('.menu-btn').on('click', function(event) {
+    $('.nav').addClass('js-nav-shadow');
+});
+
 // Scroll animation with offset to preserve spacing above headers
 $('body').on('click', '.nav a', function(event) {
     var top = $($(this).attr('href')).offset().top;
     $('html, body').stop().animate({
-        scrollTop: top - 80
+        scrollTop: top - 128
     }, 500, 'easeInOutQuad');
+    // On mobile, close the dropdown menu when an anchor is clicked
+    $('.menu-btn').prop('checked', false);
     event.preventDefault();
 });
